57da859daa5d0e35931fc69f236b9d1949965a0d
Nicolas <nicolasliendro0697@gmail.com>
2021-09-02 14:17:53 -0300

    [new] Add commands to send and listen channel

--- __init__.py
+++ __init__.py
@@ -23,26 +23,84 @@ Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
     pip install <package> -t .
 
 """
+
 base_path = tmp_global_obj["basepath"]
 cur_path = base_path + "modules" + os.sep + "Slack" + os.sep + "libs" + os.sep
 if cur_path not in sys.path:
     sys.path.append(cur_path)
 
-import os
+from slack import RTMClient
+from slack_service import SlackService
 
 """
     Obtengo el modulo que fue invocado
 """
-
+global slack_service_
 module = GetParams("module")
 
-
 if module == "connect":
-   
+    slack_token = GetParams("slack_token")
+    res = GetParams("res")
     try:
-        
+        slack_service_ = SlackService(slack_token)
+        SetVar(res, True)
+    except Exception as e:
+        SetVar(res, False)
+        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+        PrintException()
+        raise e
+
+if module == "send_message":
+    channel = GetParams("channel_id")
+    text = GetParams("message")
+    res = GetParams("res")
+    try:
+        res_status_code = slack_service_.post_message(channel, text)
+        SetVar(res, res_status_code)
     except Exception as e:
         print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
         PrintException()
         raise e
 
+if module == "list_channels":
+    res = GetParams("res")
+    try:
+        list_channels = slack_service_.list_channels('public_channel')
+        SetVar(res, list_channels)
+    except Exception as e:
+        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+        PrintException()
+        raise e
+
+@RTMClient.run_on(event="message")
+def listen_events_slack(**payload):
+    """
+        This function triggers when someone sends
+        a message on the slack
+        """
+    global message
+    global rtm_client
+    message = payload
+    rtm_client._event_loop.stop()
+    rtm_client._event_loop.close()
+
+if module == "listen_channel":
+    res = GetParams("res")
+    try:
+        rtm_client = None
+        rtm_client = RTMClient(token="xoxb-2425917630453-2436304297300-mqWaBb7VNAToVQY0FhyhbQnq")
+        print("Listening workspace")
+        try:
+            message = None
+            rtm_client.start()
+        except RuntimeError:
+            pass
+        rtm_client = None
+        if message is not None:
+            del message['rtm_client']
+            del message['web_client']
+            SetVar(res, message)
+    except Exception as e:
+        print("\x1B[" + "31;40mAn error occurred\x1B[" + "0m")
+        PrintException()
+        raise e
---
+++ libs/slack_service.py
@@ -0,0 +1,85 @@
+import requests
+from slack import RTMClient
+from slack import WebClient
+from slack.errors import SlackApiError
+
+
+def get_channel_info(channel):
+    channel_info = {
+        'id': channel['id'],
+        'name': channel['name']
+    }
+    return channel_info
+
+
+class SlackService:
+    def __init__(self, slack_token: str):
+        self.slack_token = slack_token
+        self.slack_client = WebClient(token=slack_token)
+        self.rtm_client = RTMClient(token=slack_token)
+
+    def post_message(self, channel: str, text: str) -> int:
+        try:
+            response = self.slack_client.chat_postMessage(
+                channel=channel,
+                text=text
+            )
+            return response.status_code
+        except SlackApiError as e:
+            raise e
+
+    def list_channels(self, types : str) -> list:
+        response = self.slack_client.conversations_list(types=types)
+        list_channels = response.data['channels']
+        list_ids = list(map(get_channel_info, list_channels))
+        return list_ids
+
+
+if __name__ == '__main__':
+    slack_service = SlackService('xoxb-2425917630453-2436304297300-mqWaBb7VNAToVQY0FhyhbQnq')
+    try:
+        print("empezando uwu")
+        slack_service.rtm_client.start()
+        print("Terminado 7u7")
+    except Exception as err:
+        print(err)
+
+
+# @RTMClient.run_on(event="message")
+# def amusebot(**payload):
+#     print(payload)
+#     """
+#     This function triggers when someone sends
+#     a message on the slack
+#     """
+#     data = payload["data"]
+#     web_client = payload["web_client"]
+#     bot_id = data.get("bot_id", "")
+#
+#     # If a message is not send by the bot
+#     if bot_id == "":
+#         channel_id = data["channel"]
+#
+#         # Extracting message send by the user on the slack
+#         text = data.get("text", "")
+#         text = text.split(">")[-1].strip()
+#
+#         response = ""
+#         if "help" in text.lower():
+#             user = data.get("user", "")
+#             response = f"Hi <@{user}>! I am AmuseBot :)"
+#         else:
+#             activity_json_response = requests.get("http://www.boredapi.com/api/activity/").json()
+#             activity = activity_json_response['activity']
+#             response = str(activity)
+#
+#         # Sending message back to slack
+#         web_client.chat_postMessage(channel=channel_id, text=response)
+#
+#
+# try:
+#     rtm_client = RTMClient(token="xoxb-2425917630453-2436304297300-mqWaBb7VNAToVQY0FhyhbQnq")
+#     print("Bot is up and running!")
+#     rtm_client.start()
+# except Exception as err:
+#     print(err)
